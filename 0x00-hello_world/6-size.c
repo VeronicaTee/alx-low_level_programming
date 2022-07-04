04e9c693ef777a873dba9d7df7c76b4f72589c3a
Veronica <veronicatomilola@gmail.com>
2022-07-03 18:53:46 -0700

    sz

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,10 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * main - This function prints out the size on the computer
+ * Return: Always zero
+ */
+int main(void)
+{
+	printf("we are \n");
+	return (0);
+}
