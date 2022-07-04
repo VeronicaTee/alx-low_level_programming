ed2c86a5567249162111b04f90914c3ba525e5be
Veronica <veronicatomilola@gmail.com>
2022-07-03 18:19:39 -0700

    print a string with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main - This function prints the assigned string
+ * Return: Always zero
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+}
