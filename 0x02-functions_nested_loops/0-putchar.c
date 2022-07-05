99b1c9f0610eed7347e292e3d3f800a6c30173d6
Veronica <veronicatomilola@gmail.com>
2022-07-05 09:26:39 -0700

    Write a program that prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - This function calls the _putchar funstion
+ * _putchar - This functiion prints _putchar
+ * Return: Always zero
+ */
+
+void _putchar(void);
+int main(void)
+{
+	_putchar();
+	return (0);
+}
+void _putchar(void)
+{
+	printf("_putchar");
+}
