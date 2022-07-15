3333d7224aa29e464d6b0614a7db28df8df12f24
Veronica <veronicatomilola@gmail.com>
2022-07-14 17:34:08 -0700

    strcmp fix

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
  * _strcmp - Compares pointers to two strings.
@@ -12,9 +12,32 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
+	int i, l1, l2;
 
-	while (*(s1 + i) && *(s2 + i) && (*(s1 + i) == *(s2 + i)))
+	i = 0;
+	l1 = 0;
+	l2 = 0;
+	while (s1[l1] != 0)
+	{
+		l1++;
+	}
+	while (s2[l2] != 0)
+	{
+		l2++;
+	}
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
 		i++;
-	return (*(s1 + i) - *(s2 + i));
+	}
+	if (l1 > l2)
+	{
+		return (s1[l2]);
+	}
+	else if (l1 < l2)
+	{
+		return (s2[l1] * -1);
+	}
+	return (0);
 }
