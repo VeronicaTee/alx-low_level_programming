c77806d07b09410ad7f2762fde6c61c441edceb9
Veronica <veronicatomilola@gmail.com>
2022-07-22 00:36:23 -0700

    Write a program that adds positive numbers.

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Adds two numbers and prints the result.
+ * @argc: Arguments count
+ * @argv: Array of argument strings
+ * Return: 0 for success and 1 if params are not 2..
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc == 3)
+	{
+		printf("%d\n", atoi(argv[1]) + atoi(argv[2]));
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+}
