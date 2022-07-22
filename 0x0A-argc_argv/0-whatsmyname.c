82632731ed91ac36e116eff0ff391ba20e55751a
Veronica <veronicatomilola@gmail.com>
2022-07-21 22:03:53 -0700

    Write a program that prints its name, followed by a new line.

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the program's name
+ * @argc: Arguments count
+ * @argv: Array of argument strings
+ * Return: Always zero.
+ */
+
+int main(int argc __atrribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
